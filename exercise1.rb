3d5fac6ee05823cfd2b66d4a74579cf6b8357510
Graham <grahamspence@gmail.com>
2018-07-17 15:29:01 -0400

    first exercise complete

---
+++ exercise1.rb
@@ -0,0 +1,13 @@
+# Exercise 1
+# Define a method called double that accepts an argument called my_number and returns that number multiplied by 2.
+
+# Try calling it multiple times and pass in different numbers each time.
+
+def double(my_number)
+  return my_number * 2
+end
+
+puts double(7)
+puts double(50)
+puts double(100)
+puts double(1234)
